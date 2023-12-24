41ae90326e3f0652badfe54f7b7babbdd1b1033c
Fall_zhang <zhangbofei@163.com>
2023-12-24 19:23:03 +0800

    tool(javascript): 🔧 添加 vite 作为替补打包器

---
+++ build/vite.config.js
@@ -0,0 +1,34 @@
+// vite.config.js
+// 无法导出为多个 css 文件，因此弃用
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+const pathResolve = (file) => {
+  return resolve(__dirname, '../packages/' + file)
+}
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@P': resolve(__dirname, '../packages'),
+    }
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: [pathResolve('index.js'), pathResolve('theme-dark/index.less'), pathResolve('theme-default/index.less')],
+      // name: 'MyLib',
+      // the proper extensions will be addedoutput.exports
+      // fileName: ['main', 'theme-dark', 'theme-default'],
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
