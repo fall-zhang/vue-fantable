17c08af55664dcdf3d659700cdef6e87997bbe03
fall <zhangbofei@163.com>
2024-01-09 19:21:31 +0800

    style(javascript): 🎨 添加全局事件

--- packages/ve-select/src/util/constant.js
+++ packages/ve-select/src/util/constant.js
@@ -1,12 +1,6 @@
 // prefix
 export const PREFIX_CLS = 've-select-'
 
-// emit events
-export const EMIT_EVENTS = {
-  // on select change
-  SELECT_CHANGE: 'on-select-change',
-}
-
 // comps name
 export const COMPS_NAME = {
   VE_SELECT: 'VeSelect',
