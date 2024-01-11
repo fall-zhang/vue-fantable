3ae8ef7ce610b7a4ea3e4d1f5571262c3b756758
Fall <zhangbofei@163.com>
2024-01-11 12:59:14 +0800

    tool(javascript): 🔧 更新测试和配置工具版本

---
+++ vitest.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import vue from 'unplugin-vue/vite'
+import vueJSX from '@vitejs/plugin-vue-jsx'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@P': resolve(__dirname, './packages'),
+    }
+  },
+  plugins: [
+    vue(),
+    vueJSX()
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
