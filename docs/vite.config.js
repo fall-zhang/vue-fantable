71bb8f9ab0bf8aa5a7cf69f1e9cde06366161211
Fall <zhangbofei@163.com>
2024-01-17 18:13:38 +0800

    tool(javascript): 🔧 更新打包工具

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -6,6 +6,9 @@ import { resolve as pathResolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Markdown from 'unplugin-vue-markdown/vite'
+import MarkdownItAnchor from 'markdown-it-anchor'
+import MarkdownItPrism from 'markdown-it-prism'
+import MarkdownItContainer from 'markdown-it-container'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { fileURLToPath } from 'node:url'
 
@@ -37,7 +40,61 @@ export default defineConfig({
     vue({
       include: [/\.vue$/, /\.md$/], // <-- allows Vue to compile Markdown files
     }),
-    Markdown({ /* options */ }),
+    Markdown({
+      // default options passed to markdown-it
+      // see: https://markdown-it.github.io/markdown-it/
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+      // A function providing the Markdown It instance gets the ability to apply custom settings/plugins
+      markdownItSetup(md, options) {
+        console.log('🚀 ~ markdownItSetup ~ options:', options) // undefined
+        // for example
+        md.use(MarkdownItAnchor)
+        md.use(MarkdownItPrism)
+        md.use(MarkdownItContainer, 'anchor', {
+          validate(params) {
+            return params.trim().match(/^anchor\s*(.*)$/)
+          },
+          render(tokens, idx) {
+            const m = tokens[idx].info.trim().match(/^anchor\s*(.*)$/)
+            if (tokens[idx].nesting === 1) {
+              const label = m && m.length > 1 ? m[1] : ''
+
+              return `<anchor is-edit label="${label}" fileName="${options.resourceFileName}" />
+          `
+            }
+            return ''
+          },
+        })
+        md.use(MarkdownItContainer, 'demo', {
+          validate(params) {
+            return params.trim().match(/^demo\s*(.*)$/)
+          },
+          render(tokens, idx) {
+            const m = tokens[idx].info.trim().match(/^demo\s*(.*)$/)
+            if (tokens[idx].nesting === 1) {
+              const description = m && m.length > 1 ? m[1] : ''
+              const content =
+                      tokens[idx + 1].type === 'fence'
+                        ? tokens[idx + 1].content
+                        : ''
+              return `<demo-block>
+          ${description ? `<div>${md.render(description)}</div>` : ''}
+          <!--element-demo: ${content}:element-demo-->
+          `
+            }
+            return '</demo-block>'
+          },
+        })
+        md.use(MarkdownItContainer, 'tip')
+        md.use(MarkdownItContainer, 'warning')
+      },
+      // Class names for the wrapper div
+      wrapperClasses: 'markdown-body'
+    }),
     vueJsx({
       // include: [/\.[jt]sx$/, /\.vue$/]
       // options are passed on to @vue/babel-plugin-jsx
