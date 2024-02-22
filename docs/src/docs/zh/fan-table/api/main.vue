d4d75aa00b609ad281243b1be5ced53d7a9a519b
Fall <zhangbofei@163.com>
2024-02-22 11:36:21 +0800

    docs(javascript): 📚 更新文档的引用路径以及补充一个 example

--- docs/src/docs/zh/fan-table/api/main.vue
+++ docs/src/docs/zh/fan-table/api/main.vue
@@ -44,54 +44,54 @@
     </div>
 </template>
 <script>
-import { db } from "./db";
-import TableProps from "./table-props";
-import ColumnProps from "./column-props";
-import InstanceMethods from "./instance-methods";
-import ExpandOption from "./expand-option-props";
-import CheckboxOption from "./checkbox-option-props";
-import RadioOption from "./radio-option-props";
-import VirtualScrollOption from "./virtual-scroll-option-props";
-import SortOption from "./sort-option-props";
-import CellSpanOption from "./cell-span-option-props";
-import EventCustomOption from "./event-custom-option-props";
-import ColumnHiddenOption from "./column-hidden-option-props";
-import CellStyleOption from "./cell-style-option-props";
-import RowStyleOption from "./row-style-option-props";
-import CellSelectionOption from "./cell-selection-option-props";
-import EditOption from "./edit-option-props";
-import contextmenuHeaderOption from "./contextmenu-header-option-props";
-import contextmenuBodyOption from "./contextmenu-body-option-props";
-import cellAutofillOption from "./cell-autofill-option-props";
-import clipboardOption from "./clipboard-option-props";
-import columnWidthResizeOption from "./column-width-resize-option-props";
+import { db } from './db'
+import TableProps from './table-props.vue'
+import ColumnProps from './column-props/index.vue'
+import InstanceMethods from './instance-methods.vue'
+import ExpandOption from './expand-option-props.vue'
+import CheckboxOption from './checkbox-option-props.vue'
+import RadioOption from './radio-option-props.vue'
+import VirtualScrollOption from './virtual-scroll-option-props.vue'
+import SortOption from './sort-option-props.vue'
+import CellSpanOption from './cell-span-option-props.vue'
+import EventCustomOption from './event-custom-option-props.vue'
+import ColumnHiddenOption from './column-hidden-option-props.vue'
+import CellStyleOption from './cell-style-option-props.vue'
+import RowStyleOption from './row-style-option-props.vue'
+import CellSelectionOption from './cell-selection-option-props.vue'
+import EditOption from './edit-option-props.vue'
+import contextmenuHeaderOption from './contextmenu-header-option-props.vue'
+import contextmenuBodyOption from './contextmenu-body-option-props.vue'
+import cellAutofillOption from './cell-autofill-option-props.vue'
+import clipboardOption from './clipboard-option-props.vue'
+import columnWidthResizeOption from './column-width-resize-option-props.vue'
 export default {
-    components: {
-        TableProps,
-        ColumnProps,
-        InstanceMethods,
-        CellSpanOption,
-        EventCustomOption,
-        ColumnHiddenOption,
-        CellStyleOption,
-        RowStyleOption,
-        ExpandOption,
-        CheckboxOption,
-        RadioOption,
-        VirtualScrollOption,
-        SortOption,
-        CellSelectionOption,
-        EditOption,
-        contextmenuHeaderOption,
-        contextmenuBodyOption,
-        cellAutofillOption,
-        clipboardOption,
-        columnWidthResizeOption,
-    },
-    data() {
-        return {
-            db: db,
-        };
-    },
-};
+  components: {
+    TableProps,
+    ColumnProps,
+    InstanceMethods,
+    CellSpanOption,
+    EventCustomOption,
+    ColumnHiddenOption,
+    CellStyleOption,
+    RowStyleOption,
+    ExpandOption,
+    CheckboxOption,
+    RadioOption,
+    VirtualScrollOption,
+    SortOption,
+    CellSelectionOption,
+    EditOption,
+    contextmenuHeaderOption,
+    contextmenuBodyOption,
+    cellAutofillOption,
+    clipboardOption,
+    columnWidthResizeOption,
+  },
+  data() {
+    return {
+      db,
+    }
+  },
+}
 </script>
