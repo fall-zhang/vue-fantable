8bdbc305ca75bf0e145ca6b0a46b1413c1d0e519
fall-zhang <62327825+fall-zhang@users.noreply.github.com>
2024-02-22 10:13:44 +0000

    Deploying to github-pages from @ fall-zhang/vue-fantable@5ef1b1b90ef1bdbf12b853d943047e53a241f2ea 🚀

---
+++ assets/base-usage-9hxsg7lj.js
@@ -0,0 +1,1 @@
+import{_ as o,r as n,o as t,c as a}from"./index-g2bFAzY6.js";const c={};function s(r,_){const e=n("ve-pagination");return t(),a(e,{total:600})}const i=o(c,[["render",s]]);export{i as default};
