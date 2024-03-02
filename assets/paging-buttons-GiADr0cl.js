ecc913bb30da8bb639c7ce7681605811f7068150
fall-zhang <62327825+fall-zhang@users.noreply.github.com>
2024-03-02 07:05:38 +0000

    Deploying to github-pages from @ fall-zhang/vue-fantable@56a9ec3b77caf287495346f872f030aeb4141bc7 🚀

---
+++ assets/paging-buttons-GiADr0cl.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as e,c as a}from"./index-WVU3T6XN.js";const c={};function r(s,_){const n=t("ve-pagination");return e(),a(n,{total:600,"paging-count":6})}const i=o(c,[["render",r]]);export{i as default};
