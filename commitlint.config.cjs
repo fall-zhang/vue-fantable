ca7916f20c2aab61123425d7915177595c1b1b0f
fall-zhang <zhangbofei@163.com>
2024-04-14 18:51:27 +0800

    feat(typescript): 🚀 添加类型检查命令

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -103,7 +103,7 @@ module.exports = {
       //  👾/📝
     ],
     useEmoji: true,
-    scopes: ['markdown', 'javascript', 'css', 'assets'],
+    scopes: ['markdown', 'typescript', 'css', 'assets', 'global'],
     customScopesAlign: 'bottom',
     emptyScopesAlias: 'empty',
     customScopesAlias: 'custom',
